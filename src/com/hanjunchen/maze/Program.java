505e1eb825e907f55010fca8648d00480dfa3c78
Dylan Chen <hchen004@citymail.cuny.edu>
2016-03-07 23:28:59 -0500

    Create Program.java

---
+++ src/com/hanjunchen/maze/Program.java
@@ -0,0 +1,8 @@
+package com.hanjunchen.maze;
+
+public class Program {
+	public static void main(String[] args) throws java.lang.Exception {
+		// TODO Auto-generated method stub
+		Maze maze = new Maze();
+	}
+}
